fe4d9747174715bc6a7c618a84b645f8944ded39
Huascar <huascarmc@hotmail.com>
2017-12-02 12:41:58 +0000

    added 2x1 property to product

--- app/src/main/java/com/example/huascar/shoppingbasket/Product.java
+++ app/src/main/java/com/example/huascar/shoppingbasket/Product.java
@@ -8,10 +8,12 @@ public class Product implements Item {
 
     private String name;
     private int price;
+    private boolean oneFree;
 
-    public Product(String name, int price) {
+    public Product(String name, int price, boolean oneFree) {
         this.name = name;
         this.price = price;
+        this.oneFree = oneFree;
     }
 
     public String getName() {
