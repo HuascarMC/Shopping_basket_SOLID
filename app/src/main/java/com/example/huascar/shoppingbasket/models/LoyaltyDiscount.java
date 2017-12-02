189322a36481c1bf000b59dfc718b579220e0f27
Huascar <huascarmc@hotmail.com>
2017-12-02 14:30:02 +0000

    + loyaltydiscount class

---
+++ app/src/main/java/com/example/huascar/shoppingbasket/models/LoyaltyDiscount.java
@@ -0,0 +1,10 @@
+package com.example.huascar.shoppingbasket.models;
+
+import com.example.huascar.shoppingbasket.Interfaces.Discount;
+
+/**
+ * Created by huascar on 02/12/2017.
+ */
+
+public class LoyaltyDiscount implements Discount {
+}
