2d511c6924d832baa03d21b076d37746f740ca8d
Huascar <huascarmc@hotmail.com>
2017-12-02 11:21:39 +0000

    added item interface

--- app/src/main/java/com/example/huascar/shoppingbasket/ShoppingBasket.java
+++ app/src/main/java/com/example/huascar/shoppingbasket/ShoppingBasket.java
@@ -1,8 +1,15 @@
 package com.example.huascar.shoppingbasket;
 
+import java.util.ArrayList;
+
 /**
  * Created by huascar on 02/12/2017.
  */
 
 public class ShoppingBasket {
+    private ArrayList basket;
+
+    public ShoppingBasket() {
+        ArrayList basket = new ArrayList<>();
+    }
 }
